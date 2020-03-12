b3e302e3d4809677ed1a37d8d3a53bae300ebadd
frhl <frederik.heymann@gmail.com>
2020-03-12 19:43:40 -0400

    minor fixes

--- R/map_gene_id.R
+++ R/map_gene_id.R
@@ -1,7 +1,10 @@
+#' @title map id to gene
+#' @description maps an uniprot id to hgnc symbol using a hashmap
+#' @param uniprot a vector of uniprot IDs without isoform.
+#' @return a vector of corresponding hgnc symbols
 
 map_gene_id = function(uniprot){
   require(hashmap)
   hm <- load_hashmap('inst/extdata/uniprotid_to_hgnc')
   return(hm[[uniprot]])
-  
 }
