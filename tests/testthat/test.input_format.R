8ab92a2fb8eb0ea1bdaa0ee09c235fca88d4ba05
frhl <frederik.heymann@gmail.com>
2020-03-12 14:21:13 -0400

    concrete test case

--- tests/testthat/test.input_format.R
+++ tests/testthat/test.input_format.R
@@ -1,7 +1,25 @@
-
-
 library(testthat)
 
-test_that('testing works',{
-  expect_true(1==1)
+# make artificial data
+df1 = data.frame(gene = letters,rep1=runif(26), rep2=rnorm(26))
+df2 = matrix()
+
+
+test_that('input format can describe different datastes',{
+  
+  # this contains gene and rep
+  result = input_format(df1)
+  expect_true(result$gene_rep)
+  expect_false(result$accession_rep)
+  
+  # next test ..
+  
+  
+})
+
+test_that('yields errors',{
+
+  # e.g do not accept matrix
+  expect_error(df2)
+  
 })
