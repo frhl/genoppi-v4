a76490e3e13902ab8bc9151e48e1ee985362278e
frhl <frederik.heymann@gmail.com>
2020-03-12 10:19:38 -0400

    added first package framework

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' @title test
+#' @description a function that tests functionality of the package. Will me remvoed
+#' @param x redundant parameter
+#' @author flassen
+#' @export
+#' 
+
+#.libPaths('Toolbox/rlib/')
+
+test <- function(x){
+  print('testing package!')
+}
