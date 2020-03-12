63983930b56aa4b5502a82499025616fa103e59b
frhl <frederik.heymann@gmail.com>
2020-03-12 12:39:51 -0400

    updated documentation

--- R/read_input.R
+++ R/read_input.R
@@ -1,8 +1,16 @@
 #' @title read input
 #' @description reads
 #' @param filename path to input file
+#' @param sep the field character seperator, see \code{?read.table}.
+#' @param header a logical value indicating whether the file contains the names of the variables as its first line.
 #' @export
 
-read_input <- function(filename){
+read_input <- function(filename, header, sep){
+  
+  input = read.table(filename, header, sep)
+  
+  
   return(NULL)
 }
+
+
