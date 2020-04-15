6d34bf0e2686fd92f5a32b37b10d67e67c79d477
yuhanhsu <yuhanhsu@broadinstitute.org>
2020-04-14 21:17:18 -0400

    modified map_gene_id() function to return a single df

--- R/map_gene_id.R
+++ R/map_gene_id.R
@@ -1,7 +1,7 @@
 #' @title Map Uniprot accession ID to gene name (HGNC symbol)
-#' @description Map and replace accession_number column in a data.frame with gene column. Isoforms (indicated by suffixes separated by . or - in acession_number) are mapped to the same gene names.
+#' @description Add gene column to data.frame containing accesion_number column. Isoforms (indicated by suffixes separated by . or - in acession_number) are mapped to the same gene names.
 #' @param df data.frame containing accession_number column
-#' @return list of (df with accession_number column replaced by gene column, df with accession_number and gene columns)
+#' @return data.frame consisting of input df + new gene column
 #' @export
 
 map_gene_id <- function(df){
@@ -13,11 +13,7 @@ map_gene_id <- function(df){
  
   # map accession_number in df to gene (unmapped entries shown as NA)
   matchInds <- match(accession_noIsoform,accession_gene_table$accession_number)
-  mapDf <- data.frame(accession_number=df$accession_number,gene=accession_gene_table$gene[matchInds]) 
-
-  # replace accession_number with gene in input df
-  df$accession_number <- mapDf$gene
-  colnames(df)[colnames(df)=="accession_number"] <- "gene"
+  df$gene <- as.factor(accession_gene_table$gene[matchInds])
  
-  return(list(df,mapDf)) 
+  return(df) 
 }
--- tests/testthat/test.map_gene_id.R
+++ tests/testthat/test.map_gene_id.R
@@ -2,13 +2,10 @@ context('map_gene_id')
 
 # read in test data
 df <- data.frame(accession_number=c('Q96DR7', 'Q13148', 'P17948'), rep1=1:3, rep2=4:6)
-outDf <- data.frame(gene=c('ARHGEF26','TARDBP', 'FLT1'), rep1=1:3, rep2=4:6)
-mapDf <- data.frame(accession_number=c('Q96DR7', 'Q13148', 'P17948'),
-		gene=c('ARHGEF26','TARDBP', 'FLT1'))
+outDf <- data.frame(df,gene=c('ARHGEF26','TARDBP', 'FLT1'))
 
 df2 <- data.frame(accession_number=c('Q96DR7','Q96DR7-3'))
-outDf2 <- data.frame(gene=c('ARHGEF26','ARHGEF26'))
-mapDf2 <- data.frame(accession_number=c('Q96DR7','Q96DR7-3'),gene=c('ARHGEF26','ARHGEF26'))
+outDf2 <- data.frame(accession_number=c('Q96DR7','Q96DR7-3'),gene=c('ARHGEF26','ARHGEF26'))
 
 df3 <- data.frame(accession_number=c('BADNAME'))
 
@@ -16,17 +13,15 @@ test_that('map_gene_id correctly maps acession_number (uniprot) to HGNC' ,{
   
   # typical case
   result <- map_gene_id(df)
-  expect_equal(result[[1]], outDf)
-  expect_equal(result[[2]], mapDf) 
+  expect_equal(result, outDf)
   
   # valid Uniprot ID but with isoform
   result <- map_gene_id(df2)
-  expect_equal(result[[1]], outDf2)
-  expect_equal(result[[2]], mapDf2)
+  expect_equal(result, outDf2)
   
   # invalid uniprot ID
   result <- map_gene_id(df3)
-  expect_true(is.na(result[[1]]$gene))
+  expect_true(is.na(result$gene))
   
 })
 
