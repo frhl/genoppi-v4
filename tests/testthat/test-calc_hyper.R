cb8692fe8ce69741511382813559da1df765ecb0
frhl <frederik.heymann@gmail.com>
2020-04-15 14:18:41 +1000

    covr now reports coverage correctly

--- tests/testthat/test-calc_hyper.R
+++ tests/testthat/test-calc_hyper.R
@@ -15,15 +15,15 @@ geneDf <- geneInput[[1]]
 intersectDf <- geneInput[[2]]
 
 test_that('calc_hyper can return correct overlap results',{
-
+  
   # InWeb
   result <- calc_hyper(sigDf,inwebDf,inwebInterDf,"BCL2")
   expect_equal(format(result[[1]]$pvalue,digits=3),"0.908")
   expect_equal(result[[2]][["InWeb"]]$successInSample_genes,"LARP1")
-
+  
   # gene list
   result <- calc_hyper(sigDf,geneDf,intersectDf,"BCL2")
   expect_equal(format(result[[1]]$pvalue,digits=3),"0.00513")
   expect_identical(sort(result[[2]][["ALS"]]$successInSample_genes),c("ATXN2","FUS","PFN1","TAF15"))
-
+  
 })
